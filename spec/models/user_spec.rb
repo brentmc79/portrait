be5b5f22487d7a9163a10bf3a882b5d9f9b417f5
Brent Collier <brentmc79@gmail.com>
2014-01-04 23:07:11 -0800

    Add user specs for password/email validations

--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -4,19 +4,19 @@ describe User, 'authentication' do
   it 'should authenticate a valid user' do
     User.authenticate('jordan', 'password').should == users(:jordan)
   end
-  
+
   it 'should not authenticate valid user with wrong password' do
     User.authenticate('jordan', 'wrong').should be_nil
   end
-  
+
   it 'should not authenticate valid user with nil password' do
     User.authenticate('jordan', nil).should be_nil
   end
-  
+
   it 'should not authenticate with invalid user name' do
     User.authenticate('invalid', 'anything').should be_nil
   end
-  
+
   it 'should not authenticate with nil user name' do
     User.authenticate(nil, nil).should be_nil
   end
@@ -26,15 +26,26 @@ describe User, 'validations' do
   it 'should have a name' do
     User.new.should have(1).error_on(:name)
   end
-  
+
+  it 'should have an email' do
+    User.new.should have(1).error_on(:email)
+  end
+
   it 'should have a name with valid characters' do
     User.new(name: 'INVALID').should have(1).error_on(:name)
   end
-  
+
   it 'should have a password' do
     User.new.should have(1).error_on(:password)
   end
-  
+
+  it 'should not need a password on update' do
+    user = users(:jordan)
+    user.password = nil
+    user.valid?
+    user.should have(0).errors_on(:password)
+  end
+
   it 'should have a unique name' do
     User.new(name: users('jordan').name).should have(1).error_on(:name)
   end
