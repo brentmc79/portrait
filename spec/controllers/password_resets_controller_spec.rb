44296033b518031bbb8e4971bd21cd1fd980eef2
Brent Collier <brentmc79@gmail.com>
2014-01-05 01:04:00 -0800

    Adding some password reset controller specs

--- spec/controllers/password_resets_controller_spec.rb
+++ spec/controllers/password_resets_controller_spec.rb
@@ -9,4 +9,24 @@ describe PasswordResetsController do
     end
   end
 
+  describe 'POST create' do
+    it 'is successful' do
+      expect {
+        post :create, email: 'jordan@email.com'
+      }.to change{ActionMailer::Base.deliveries.size}.by(1)
+    end
+
+    it 'redirects on success' do
+      post :create, email: 'jordan@email.com'
+      response.should redirect_to(root_url)
+    end
+
+    it 'fails silently with an invalid email' do
+      expect {
+        post :create, email: 'nadroj@email.com'
+      }.not_to change{ActionMailer::Base.deliveries.size}
+      response.should redirect_to(root_url)
+    end
+  end
+
 end
